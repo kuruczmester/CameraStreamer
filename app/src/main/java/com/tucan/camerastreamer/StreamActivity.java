510768459101825865d9f539e9880a85ce1bc387
akurucz <kurucz.mester@gmail.com>
2016-03-26 00:49:55 +0100

    add Cast button

--- app/src/main/java/com/tucan/camerastreamer/StreamActivity.java
+++ app/src/main/java/com/tucan/camerastreamer/StreamActivity.java
@@ -3,13 +3,25 @@ package com.tucan.camerastreamer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+
 public class StreamActivity extends AppCompatActivity {
+
+    private MediaRouteSelector mediaRouteSelector;
+    private MediaRouter mediaRouter;
+    private MediaRouter.Callback mediaRouterCallback;
+    private CastDevice selectedDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +38,62 @@ public class StreamActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast("YOUR_APPLICATION_ID"))
+                .build();
+        mediaRouterCallback = new CustomMediaRouterCallback();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mediaRouter.addCallback(mediaRouteSelector, mediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
+    protected void onStop() {
+        mediaRouter.removeCallback(mediaRouterCallback);
+        super.onStop();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_stream, menu);
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mediaRouteSelector);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    class CustomMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            selectedDevice = CastDevice.getFromBundle(info.getExtras());
+            String routeId = info.getId();
+
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+//      TODO: teardown();
+            selectedDevice = null;
+        }
+    }
+
 }
